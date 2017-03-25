40992159c011b5a6ee8d8d2404244501242d9692
Andrew Seigner <andrew@sig.gy>
2017-03-25 04:33:52 -0700

    emit orientation and motion events

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # eurythmic
 
-Harmonious devices.
+Harmonious devices. Inspired by https://github.com/MylesBorins/harmonic
 
 ## Setup
 
 ```bash
 npm install
 ```
+
+## Run
+
+```bash
+node index.js
+```
+
+- Open http://[LOCAL_IP]:3000 on a phone
+- Open http://localhost:3000/voltron locally
--- index.js
+++ index.js
@@ -4,14 +4,6 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var compression = require('compression');
-
-app.use(compression());
-
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 var clients = [];
@@ -20,20 +12,17 @@ var voltron = io.of('/voltron');
 voltron.on('connection', function(socket){
   console.log('voltron connected');
 });
-voltron.emit('ondevicemotion', 'SUP');
 
 io.on('connection', function(socket){
   clients.push(socket);
 
-  console.log('a user connected ' + socket.id);
-  // console.log('THIS');
-  // console.log(this);
+  console.log('a user connected: ' + socket.id);
 
   socket.on('ondevicemotion', function(motion) {
-    console.log(this.id);
+    console.log(this.id + ": " + JSON.stringify(motion));
 
+    motion.socket = socket.id;
     voltron.emit('ondevicemotion', motion);
-    // console.log(motion);
   });
 });
 
